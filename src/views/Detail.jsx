cafab939fa91683d126c0f8c8485834f0fcf4196
wr <2119495210@qq.com>
2023-11-24 16:01:40 +0800

    P96 已完成

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { LeftOutline, MessageOutline, LikeOutline, StarOutline, MoreOutline } from 'antd-mobile-icons';
+import { Badge, Toast } from 'antd-mobile';
 
-import './Detail.less'
 
-export default function Detail() {
+import "./Detail.less";
+
+export default function Detail(props) {
+  let {navigate} = props;
   return (
-    <div className='detail-box'>Detail</div>
-  )
+    <div className="detail-box">
+      <div className="content"></div>
+      {/* 底部图标 */}
+      <div className="tab-bar">
+        <div className="back">
+          <LeftOutline onClick={()=>{navigate(-1)}}/>
+        </div>
+        <div className="icons">
+          <Badge content={666}>
+            <MessageOutline />
+          </Badge>
+          <Badge content={666}>
+            <LikeOutline />
+          </Badge>
+          <span className={"stored"}>
+            <StarOutline />
+          </span>
+          <span>
+            <MoreOutline />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
