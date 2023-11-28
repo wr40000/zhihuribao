224ad3131b061ce056f7fbdfeac9af7c4c7f8b3b
wr <2119495210@qq.com>
2023-11-28 20:55:17 +0800

    P101已完成

--- src/components/NavBarAgain/index.jsx
+++ src/components/NavBarAgain/index.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { NavBar } from "antd-mobile";
 import PropTypes from 'prop-types'
+import { useNavigate, useLocation, useSearchParams} from 'react-router-dom'
 
 import './index.less'
 
 export default function NavBarAgain(props) {
   const {title} = props;
-  const handleback = () => console.log("NavBar的点击回调被触发了");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [usp] = useSearchParams();
+
+  const handleback = () => {
+    // 特殊 : 登录页 & to的值是/detail/XXX
+    let to = usp.get('to');
+    // console.log(/^\/detail\/\d+$/.test(to));
+    if(location.pathname == 'login' && /^\/detail\/\d+$/.test(to)){
+      navigate(to, {replace:true})
+      return;
+    }
+    navigate(-1);
+  };
 
   return <NavBar className="navbar-again-box" onBack={handleback}>{title}</NavBar>;
 }
